723ee2d524cc00caf2e7c005b2cd1da42c5ab6ce
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-01-28 11:04:29 -0300

    Business Layer

--- src/main/java/com/amigoscode/student/StudentController.java
+++ src/main/java/com/amigoscode/student/StudentController.java
@@ -1,7 +1,5 @@
 package com.amigoscode.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+	private final StudentService service;
+
+	public StudentController(StudentService service) {
+		this.service = service;
+	}
+
 	@GetMapping
 	public List<Student> getStudents() {
-		return List.of(
-					new Student(1L, "Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21)
-				);
+		return service.getStudents();
 	}
-	
+
 }
