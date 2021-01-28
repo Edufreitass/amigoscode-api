d515b646ed6310d357bfc93da7eae8adc330a802
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-01-28 13:25:35 -0300

    Updating Student

--- src/main/java/com/amigoscode/student/StudentService.java
+++ src/main/java/com/amigoscode/student/StudentService.java
@@ -1,10 +1,12 @@
 package com.amigoscode.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
@@ -34,6 +36,27 @@ public class StudentService {
 			throw new IllegalStateException("student with id " + studentId + " does not exists");
 		}
 		repository.deleteById(studentId);
+	}
+
+	@Transactional
+	public void updateStudent(Long studentId, String name, String email) {
+		Student student = repository.findById(studentId)
+					.orElseThrow(() -> new IllegalStateException("student with id " + studentId + " does not exists")
+				);
+		
+		if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+			student.setName(name);
+		}
+		
+		if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+			Optional<Student> studentOptional = repository.findStudentByEmail(email);
+			if (studentOptional.isPresent()) {
+				throw new IllegalStateException("email taken");
+			}
+			student.setEmail(email);
+		}
+		
+		
 	}
 
 }
