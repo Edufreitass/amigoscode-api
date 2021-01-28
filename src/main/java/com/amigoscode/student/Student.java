7d6f1877a863380f0d53d80a64691b165e1b28dc
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-01-28 11:42:43 -0300

    @Transient

--- src/main/java/com/amigoscode/student/Student.java
+++ src/main/java/com/amigoscode/student/Student.java
@@ -1,6 +1,7 @@
 package com.amigoscode.student;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table
@@ -27,24 +29,24 @@ public class Student {
 	private String name;
 	private String email;
 	private LocalDate dateOfBirth;
+	
+	@Transient
 	private Integer age;
 
 	public Student() {
 	}
 
-	public Student(Long id, String name, String email, LocalDate dateOfBirth, Integer age) {
+	public Student(Long id, String name, String email, LocalDate dateOfBirth) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.dateOfBirth = dateOfBirth;
-		this.age = age;
 	}
 
-	public Student(String name, String email, LocalDate dateOfBirth, Integer age) {
+	public Student(String name, String email, LocalDate dateOfBirth) {
 		this.name = name;
 		this.email = email;
 		this.dateOfBirth = dateOfBirth;
-		this.age = age;
 	}
 
 	public Long getId() {
@@ -80,7 +82,7 @@ public class Student {
 	}
 
 	public Integer getAge() {
-		return age;
+		return Period.between(this.dateOfBirth, LocalDate.now()).getYears();
 	}
 
 	public void setAge(Integer age) {
